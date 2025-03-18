f314a89dedd1f6dcf3690fc228f9e62c0b41bd7b
Aquhil <245122748302@mvsrec.edu.in>
2025-03-18 12:58:14 +0530

    Add files via upload

---
+++ python simple.py
@@ -0,0 +1,17 @@
+# Declare two numbers
+num1 = 10
+num2 = 5
+
+# Perform arithmetic operations
+sum_result = num1 + num2
+difference_result = num1 - num2
+product_result = num1 * num2
+quotient_result = num1 / num2
+
+# Print the results
+print("Sum:", sum_result)
+print("Difference:", difference_result)
+print("Product:", product_result)
+print("Quotient:", quotient_result)
+
+
