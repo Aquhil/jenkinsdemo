6d3ca6737a06442c9313060e32ef064f7ca993e4
Aquhil <245122748302@mvsrec.edu.in>
2025-03-18 12:50:36 +0530

    Add files via upload

---
+++ ArithmeticOperations.java
@@ -0,0 +1,15 @@
+class ArithmeticOperations{
+    public static void main(String args[]){
+    int n1=10,n2=5;
+    int sum,difference,product,quotient;
+    sum=n1+n2;
+    difference=n1-n2;
+    product=n1*n2;
+    quotient=n1/n2;
+    System.out.println("Sum" +sum);
+    System.out.println("Diff" +difference);
+    System.out.println("product" +product);
+    System.out.println("Quotient" +quotient);
+    }
+    }
+     
